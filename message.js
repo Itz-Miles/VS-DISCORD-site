8f4026f7dd2ae2f22d380a0510685e32bbbfe629
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2023-05-12 11:16:23 -0600

    handle enter key + some broken build

--- message.js
+++ message.js
@@ -1,4 +1,8 @@
-
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    redirectToExecutable();
+  }
+}
 function redirectToExecutable() {
     const messageBox = document.getElementById("message-box");
     const message = messageBox.value.toLowerCase().trim(); //lower it, just in-case
